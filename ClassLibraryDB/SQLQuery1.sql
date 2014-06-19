e1e04687c3d945fd7b48452f874900fdcf1266a9
abrugnerotto <jerome.phillot@gmail.com>
2014-06-19 20:55:35 +0200

    test

---
+++ ClassLibraryDB/SQLQuery1.sql
@@ -0,0 +1,55 @@
+--Creation d'une table produits
+--Se connecter avec: >sqlplus /nolog
+--Exécution du script
+-- @c:\users\dt\desktop\test\creatPieces.sql
+
+-- sqlplus /nolog @c:\users\dt\desktop\test\creatPieces.sql
+
+--login admin
+--CONNECT system/root;
+
+--suppression des utilisateurs
+--DROP USER ecole CASCADE;
+
+--creation des utilisateurs
+--CREATE USER ecole --IDENTIFIED BY ecole;
+--CREATE USER rene --IDENTIFIED BY rene;
+
+--Ajout des privilèges
+--GRANT CONNECT, RESOURCE to ecole;
+--GRANT CONNECT, RESOURCE to rene;
+
+--Connexion utillisateur
+--CONNECT ecole/ecole;
+
+CREATE TABLE Eleves
+(
+		numEleve INT PRIMARY KEY,
+		nomEleve VARCHAR(20) NOT NULL UNIQUE,
+		motDePasse VARCHAR(20)
+);
+
+CREATE TABLE Modules
+(
+		numModule INT PRIMARY KEY,
+		nomModule VARCHAR(20) NOT NULL UNIQUE,
+		nbMatiere INT
+);
+
+CREATE TABLE Matieres(
+		numMatiere INT PRIMARY KEY,
+		nomMatiere VARCHAR(20) NOT NULL UNIQUE,
+		nbNote INT,
+		poidsMatiere INT
+		numModule INT,
+		FOREIGN KEY (numModule) REFERENCES Modules(numModule)
+);
+
+CREATE TABLE Notes(
+		numNotes INT PRIMARY KEY,
+		note VARCHAR(20) NOT NULL,
+		poidsNote INT,
+		numMatiere INT
+		FOREIGN KEY (numMatiere) REFERENCES Matieres(numMatiere)
+);
+
